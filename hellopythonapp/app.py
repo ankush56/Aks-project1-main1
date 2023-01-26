8510b7a1f337b00b2451e925ba8ea35d68cde71e
ankush56 <walia_77@outlook.com>
2023-01-25 22:50:42 -0500

    fix

--- hellopythonapp/app.py
+++ hellopythonapp/app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#######################################################
+# This section writes hello world on persistent volume mounted to containers
 #Persistent storage use
 # Get the path of the PV from an environment variable
 # set DATA_PATH env var as path to which pvc is mounted
@@ -13,12 +15,12 @@ with open(f'{data_path}/mydata.txt', 'w') as f:
 
 with open(f'{data_path}/mydata.txt', 'r') as f:
     print(f.read())
-
+#########################################################
 
 @app.route("/")
 def hello():
     return render_template('index.html', message="Superman")
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(host='0.0.0.0', port=8000)
 
